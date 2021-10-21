7c4b0870fbe32e4a9d2b3244bfb06a66226c43ad
Stephon Kumar <stephon.kumar13@myhunter.cuny.edu>
2021-10-21 16:20:15 -0400

    addR Function updated
    adding a row

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 let numRows = 0;
 let numCols = 0;
-let colorSelected; 
+let colorSelected;
 
 //Add a row
 function addR() {
     alert("Clicked Add Row")
+//Does columns == 0
+    if(numCols == 0)
+      numCols++
+//Insert Row
+    var row = grid.insertRow()
+    grid.appendChild(row);
+//Loop the columns
+    for(var i = 0; i < numCols; i++){
+      var newCell = rpw.insertCell(i);
+      var newText = document.createTextNode('CELL');
+      newCell.appendChild(newText);
+      newCell.onclick = function(){
+        return changeColor(this.parentElement.rowIndex, this.cellIndex+1)
+      }
+    }
+    numRows++;
 }
 //Add a column
 function addC() {
